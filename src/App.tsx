db6de2bbf883ec06b73b7941fbb130c9d2121c68
Patrick O'Connor <pattyo20@optonline.netm>
2023-08-11 16:44:19 -0400

    added a backend folder and failed at getting postgreSQL set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homefeed from "../src/components/Homefeed/Homefeed";
 import Profile from "../src/components/Profile/Profile";
