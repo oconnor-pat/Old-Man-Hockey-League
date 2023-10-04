35bea522d1838e4b64cb7cc9986b75e201d0ac48
Patrick O'Connor <pattyo20@optonline.net>
2023-10-04 14:25:51 -0400

    backend stuff

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const express = require('express');
+const express = require("express");
+const { Pool } = require("pg");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "postgres",
+  port: 5432,
+});
+
 // Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 // Start the server
