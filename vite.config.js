fad9959cfe78cf0a322d1ce1a643b911bb69989a
Patrick O'Connor <pattyo20@optonline.net>
2023-11-04 15:17:58 -0400

    updated vite.config.js to specify port

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    port: process.env.PORT || 8000,
+  },
 });
