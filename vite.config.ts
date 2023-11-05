17fec0374dd1fc92a44c15cd4ed7645e53b1d9aa
Patrick O'Connor <pattyo20@optonline.net>
2023-11-05 00:32:23 -0400

    forgot the vite.config files

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT),
+    port: Number(process.env.PORT || 8000),
   },
 });
