4d420995fcd2e6e3244eb1a37e5553f1a19f90f6
Patrick O'Connor <pattyo20@optonline.net>
2023-11-04 22:11:37 -0400

    removed all references to specific ports

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT) || 8000,
+    port: Number(process.env.PORT),
   },
 });
