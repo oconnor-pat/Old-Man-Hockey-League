cbeb861492e06464d7f489d0b57a3408ca8eb9ae
Patrick O'Connor <pattyo20@optonline.net>
2023-11-07 00:10:24 -0500

    set viteconfig to prod

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    mode: 'production',
+  },
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000,
