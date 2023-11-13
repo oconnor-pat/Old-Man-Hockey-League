b1ae4cd198dc4fd479dced328d4d9f62b628fe7e
Patrick O'Connor <pattyo20@optonline.net>
2023-11-12 21:48:17 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   build: {
     mode: 'production',
+    outDir: 'dist', // Specify the output directory for the build
+    rollupOptions: {
+      input: 'src/index.tsx', // Adjust this based on your project structure
+    },
   },
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   },
 });
